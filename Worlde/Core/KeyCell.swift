7c152f91433d9da9d6677491f45d16a1b9f81b91
paulshnurenko <paulsnurenko@gmail.com>
2024-03-02 18:46:47 +0100

    Code Update

---
+++ Worlde/Core/KeyCell.swift
@@ -0,0 +1,12 @@
+//
+//  KeyCell.swift
+//  Worlde
+//
+//  Created by Pavlo Shnurenko on 02.03.2024.
+//
+
+import UIKit
+
+class KeyCell: UICollectionViewCell {
+    static let identifier = "KeyCell"
+}
--- Worlde/Core/KeyboardViewController.swift
+++ Worlde/Core/KeyboardViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class KeyboardViewController: UIViewController {
+class KeyboardViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    let letters = ["qwertyuiop","asdfghjkl","zxcvbnm"]
+    private var keys: [[Character]] = []
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 2
+        let collectionVIew = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionVIew.translatesAutoresizingMaskIntoConstraints = false
+        collectionVIew.backgroundColor = .yellow
+        collectionVIew.register(KeyCell.self, forCellWithReuseIdentifier: KeyCell.identifier)
+        return collectionVIew
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
-    
+}
 
+extension KeyboardViewController {
+    
 }
